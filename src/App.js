4ab24318035b3b049f4c088044657a9b74301e06
Zedd <>
2019-05-26 13:03:45 +0200

    initial implementation

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Websocket from 'react-websocket';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      clients: [],
+      connections:{}
+    };
+  }
+  handleData(data) {
+    const {type, payload} = JSON.parse(data);
+    const state = Object.assign({}, this.state);
+    switch (type) {
+      case "CLIENT_LIST":
+          state.clients = payload;
+          this.setState(state);
+        break;
+      case "LIST_CONNECTIONS_RESPONSE":
+          state.connections = payload;
+          this.setState(state);
+        break;
+      default:
+        console.error('UNKNOWN SOCKET DATA TYPE', data);
+    }
+  }
+  sendMessage(message){
+    this.webSocket.sendMessage(message);
+  }
+  render(){
+    return (
+      <div className="App">
+        <div>
+          <button onClick={() => this.sendMessage(JSON.stringify({
+            type: "LIST_CONNECTIONS_REQUEST"
+          }))}>List Devices</button>
+          {Object.entries(this.state.connections).map(entry => (<div key={entry[0]}>{entry[1]}<button onClick={() => this.sendMessage(JSON.stringify({
+			type: "CONNECT",
+			payload: entry[0]
+		}))}>connect</button></div>))}
+        </div>
+        <div>
+          <div>Client List</div>
+          {this.state.clients.map((client, id) => (<div key={id}>{client}</div>))}
+        </div>
+
+	<Websocket url={`ws://${window.location.hostname}:5050/`}
+          onMessage={this.handleData.bind(this)}
+          ref={socket => { this.webSocket = socket; }}/>
+      </div>
+    );
+  }
+}
+
+export default App;
